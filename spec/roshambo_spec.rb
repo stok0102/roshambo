4c425b473ff1c50abdb03ce187a507ee974d27ca
Brian Grant and Caleb Stokka <brian@>
2016-06-29 10:34:30 -0700

    set up directory

---
+++ spec/roshambo_spec.rb
@@ -0,0 +1,14 @@
+require ('rspec')
+require ('roshambo')
+
+describe('String#roshambo') do
+  it("returns true if rock is the object and scissors is the argument") do
+    expect("rock".beats?("scissors")).to(eq(true))
+  end
+  it("returns true if scissors is the object and paper is the argument") do
+    expect("scissors".beats?("paper")).to(eq(true))
+  end
+  it("returns true if paper is the object and rock is the argument") do
+    expect("paper".beats?("rock")).to(eq(true))
+  end
+end
